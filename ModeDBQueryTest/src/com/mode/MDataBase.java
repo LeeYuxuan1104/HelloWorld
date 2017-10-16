f0ba79f756a56fd10ddd1c71a495541320d1acd0
LeeYuxuan1104 <1948474964@qq.com>
2017-10-17 01:34:13 +0800

    数据库模块第02次提交 李聿轩 20171017

---
+++ ModeDBQueryTest/src/com/mode/MDataBase.java
@@ -0,0 +1,43 @@
+package com.mode;
+
+import java.util.ArrayList;
+
+import com.tool.MConfig;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MDataBase extends SQLiteOpenHelper{
+
+	private ArrayList<String> list;
+	private MConfig 		  mConfig;
+	
+	public MDataBase(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+	//	数据库管理类的构造函数;
+	public MDataBase(Context context, String name, int version) {
+		super(context, name, null, version);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		mConfig=new MConfig();
+		list=mConfig.getListSQlInit();
+		for(String query:list){
+			db.execSQL(query);
+		}
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- ModeDBQueryTest/src/com/view/MainActivity.java
+++ ModeDBQueryTest/src/com/view/MainActivity.java
@@ -1,56 +1,34 @@
 package com.view;
 
 
+import java.util.ArrayList;
+
 import com.example.modetest.R;
-import com.tool.MDbHelper;
-import com.tool.MSqliteHelper;
+//import com.tool.MDbHelper;
+import com.tool.MDBHelper;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-import android.view.Menu;
+
 
 public class MainActivity extends Activity {
 	private Context			mContext;
 	
-	private MSqliteHelper	mSqLiteHelper;//  数据库帮助类;
-	private SQLiteDatabase 	mDB;		  //  数据库件;
-	private Cursor 		   	mCursor;      //  数据库遍历签;
-	
+	private MDBHelper		mdbHelper;//  数据库帮助类;
+	private ArrayList<String[]> list;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		this.mContext=MainActivity.this;
-		mSqLiteHelper	=	new MSqliteHelper(mContext);
-		mDB 			= 	mSqLiteHelper.getmDB();
-		doLoadData();
-//		mDB.execSQL("select * from can_message");
-		
-	}
-//	信息加载;
-	private void doLoadData(){
-		String 
-		sql		=	"select * from can_message";
-		mCursor	= 	mDB.rawQuery(sql, null);
-		while (mCursor.moveToNext()) {	
-			String _id=mCursor.getString(0).toString();
-			String bo_flag=mCursor.getString(1).toString();
-			String id=mCursor.getString(2).toString();
-			String message_name=mCursor.getString(3).toString();
-			String dlc=mCursor.getString(4).toString();
-			String node_name=mCursor.getString(5).toString();
-			Log.i("MyLog", "["+_id+"]["+bo_flag+"]["+id+"]["+message_name+"]["+dlc+"]["+node_name+"]");
-		}
-		if(mCursor!=null){
-			mCursor.close();
-		}
-		
-//		tvShow.setText(sResult);		
-	}
+		mdbHelper	=	new MDBHelper(mContext);
+		list		=	mdbHelper.query("select _id,id from can_message");
 
+		for(String[] strs:list){
+			Log.i("MyLog", strs[0]+"|"+strs[1]);
+		}		
+	}
 }
